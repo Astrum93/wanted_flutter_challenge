99a2e4efb5af9c7265f609a7222d22620afa9ddd
Astrum93 <jungmin2670@gmail.com>
2024-05-16 22:46:10 +0900

    home screen에 AppBar 및 TodoListView, floatingActionButton 생성

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,10 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wanted_flutter_challenge/presentation/todo/add_todo_screen.dart';
+import 'package:wanted_flutter_challenge/presentation/todo/todo_list_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Riverpod Todo App'),
+        ),
+        body: const TodoListView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const AddTodoScreen(),
+              ),
+            );
+          },
+          child: const Icon(Icons.add),
+        ));
+    ;
   }
 }
