8e213c2a56951cfd45a06be8c1ef420a1819e471
Astrum93 <jungmin2670@gmail.com>
2024-05-17 22:04:14 +0900

    todo_list_view에 생성한 todo 객체를 보여줄 ListView 생성

--- lib/presentation/todo/todo_list_view.dart
+++ lib/presentation/todo/todo_list_view.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wanted_flutter_challenge/presentation/todo/todo_detail_screen.dart';
+import 'package:wanted_flutter_challenge/provider/todo_list_provider.dart';
 
 class TodoListView extends ConsumerWidget {
   const TodoListView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final list = ref.watch();
+    final list = ref.watch(todoListProvider);
 
     if (list.isEmpty) {
       return const EmptyTodoListView();
     }
-    return const Placeholder();
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        final todo = list[index];
+        return ListTile(
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => TodoDetailScreen(
+                id: todo.id,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
 
